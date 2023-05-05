dbf932173f2447a582c5850270de742c5af36b06
msalakhedenov <m.salakhedenov@gmail.com>
2023-05-05 10:18:33 +0600

    feat(): implement signup

--- src/main/java/com/demo/todo/controller/AuthController.java
+++ src/main/java/com/demo/todo/controller/AuthController.java
@@ -1,13 +1,22 @@
 package com.demo.todo.controller;
 
 import com.demo.todo.dto.SignupRequestDto;
+import com.demo.todo.enums.CreateUserResult;
+import com.demo.todo.service.AppUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+
+  private final AppUserService userService;
+
+  public AuthController(AppUserService userService) {
+    this.userService = userService;
+  }
 
   @GetMapping("/login")
   public String loginPage() {
@@ -20,9 +29,12 @@ public class AuthController {
   }
 
   @PostMapping("/signup")
-  public String signup(@ModelAttribute SignupRequestDto signupRequest, Model model) {
+  public String signup(@ModelAttribute SignupRequestDto signupRequest, RedirectAttributes redirectAttributes) {
+    CreateUserResult result = userService.createUser(signupRequest);
 
-    return "redirect:auth/signup";
+    redirectAttributes.addFlashAttribute("result", result);
+
+    return "redirect:/auth/signup";
   }
 
 }
